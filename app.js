560a75c9fde783c3fea4df64f6f3743fc4fe6bbb
xanddybr <xanddybr@gmail.com>
2025-02-19 04:27:21 -0300

    issues fix

--- app.js
+++ app.js
@@ -1,28 +1,27 @@
 const nodemailer = require('nodemailer');
 
 const transporter = nodemailer.createTransport({
-  service: "Gmail",
-  host: "smtp.gmail.com",
-  port: 465,
-  secure: true,
+  host: "smtp.hostinger.com", //server SMTP
+  port: 587,
+  secure: false,
   auth: {
-    user: "xanddybr@gmail.com",
-    pass: "ojbe skqv holy scrb", 
+    user: "contato@catarseie.com.br",
+    pass: "Nanny@18", // PASSWORD OF GOOGLE XANDDYBR- ojbe skqv holy scrb
   },
 });
 
 async function main() {
   // send mail with defined transport object
   const info = await transporter.sendMail({
-    from: '"Catarse Treinament!" <xanddybr@gmail.com>', // sender address
-    to: "alessouza@live.com, xanddybr@gmail.com", // list of receivers
+    from: '"Catarse I.E" <contato@catarseie.com.br>', // sender address
+    to: "alessouza@live.com", // list of receivers
     subject: "Sua apostila de brinde acaba de chegar!", // Subject line
     text: "", // plain text body
-    html: "<h1>Hello Guy!</h1><br><br> Follow this link for download your gift <br><br>  https://smallpdf.com/file#s=c72b24c0-f00c-4f8d-9d6a-06503a87ce80", // html body
+    html: "<h1>Ola!</h1><br><br> Você acaba de receber em anexo o seu brinde, a lei a com calma e atneção para, e começe hoje mmesmo a dar os primeiros passos na sua tranformação pessoal! <br><br>  https://smallpdf.com/file#s=c72b24c0-f00c-4f8d-9d6a-06503a87ce80", // html body
     attachments:  [{ filename: "catarseie_gift.pdf", path: "./catarseie_gift.pdf" }],
   });
 
-  console.log("Message sent: %s", info.messageId + " " + info.response);
+  console.log(info.messageId + " - " + info.response);
   
 }
 
